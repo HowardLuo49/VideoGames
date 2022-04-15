39b6ba7e0768b0a6c7e06e7530028ba64065c5bb
Howard Luo <103543580+HowardLuo49@users.noreply.github.com>
2022-04-15 12:40:39 -0400

    Create GW.java

---
+++ Swgoh/GW.java
@@ -0,0 +1,223 @@
+
+public class GW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String names = "DaaylaBREAK" + 
+				"DackbarBREAK" + 
+				"DpiettBREAK" + 
+				"DahsokaBREAK" + 
+				"DfulcrumBREAK" + 
+				"DholdoBREAK" + 
+				"DarcBREAK" + 
+				"DasaajBREAK" + 
+				"DaurraBREAK" + 
+				"Db1BREAK" + 
+				"Db2BREAK" + 
+				"DbarissBREAK" + 
+				"DbastilaBREAK" + 
+				"DdbastilaBREAK" + 
+				"DbazeBREAK" + 
+				"Dbb8BREAK" + 
+				"DbiggsBREAK" + 
+				"DbistanBREAK" + 
+				"DbobaBREAK" + 
+				"DbodiBREAK" + 
+				"DbosskBREAK" + 
+				"D3poBREAK" + 
+				"DcadBREAK" + 
+				"DcanderousBREAK" + 
+				"DcaptainhanBREAK" + 
+				"DphasmaBREAK" + 
+				"DcaraBREAK" + 
+				"DcarthBREAK" + 
+				"DcassianBREAK" + 
+				"DcodyBREAK" + 
+				"DchewieBREAK" + 
+				"DchirpaBREAK" + 
+				"DnebbitBREAK" + 
+				"DchirrutBREAK" + 
+				"DchopperBREAK" + 
+				"DseargentBREAK" + 
+				"DcwchewieBREAK" + 
+				"DstarkBREAK" + 
+				"DclsBREAK" + 
+				"DcupBREAK" + 
+				"DdookuBREAK" + 
+				"DechoBREAK" + 
+				"DfivesBREAK" + 
+				"DyawderBREAK" + 
+				"DrexBREAK" + 
+				"DmalakBREAK" + 
+				"DmaulBREAK" + 
+				"DnihilusBREAK" + 
+				"DdrevanBREAK" + 
+				"DsidiousBREAK" + 
+				"DsionBREAK" + 
+				"DvaderBREAK" + 
+				"DtrayaBREAK" + 
+				"DdathchaBREAK" + 
+				"DdeathtrooperBREAK" + 
+				"DdengarBREAK" + 
+				"DkrennicBREAK" + 
+				"DdroidekaBREAK" + 
+				"Deeth kothBREAK" + 
+				"DemboBREAK" + 
+				"DpalpBREAK" + 
+				"DnestBREAK" + 
+				"DelderBREAK" + 
+				"DscoutBREAK" + 
+				"DezraBREAK" + 
+				"DfinnBREAK" + 
+				"Ddeath pandaBREAK" + 
+				"DfooBREAK" + 
+				"DfostostotsBREAK" + 
+				"DfostBREAK" + 
+				"DfotfpBREAK" + 
+				"DgammoreanBREAK" + 
+				"DsuxonBREAK" + 
+				"DzebBREAK" + 
+				"DgrievousBREAK" + 
+				"DhuxBREAK" + 
+				"DkenobiBREAK" + 
+				"DgasBREAK" + 
+				"DveersBREAK" + 
+				"DalphaBREAK" + 
+				"DsoldierBREAK" + 
+				"DspyBREAK" + 
+				"DthrawnBREAK" + 
+				"DyawderBREAK" + 
+				"DtarkinBREAK" + 
+				"DgreedoBREAK" + 
+				"DgreefBREAK" + 
+				"DhanBREAK" + 
+				"DheraBREAK" + 
+				"DhodaBREAK" + 
+				"DhkBREAK" + 
+				"DhothscoutBREAK" + 
+				"DhothsoldierBREAK" + 
+				"DmagnaguardBREAK" + 
+				"Dig86BREAK" + 
+				"Dig88BREAK" + 
+				"DimagundieBREAK" + 
+				"DipdBREAK" + 
+				"DsupercommandoBREAK" + 
+				"DjangoBREAK" + 
+				"DjawaBREAK" + 
+				"DjawaengineerBREAK" + 
+				"DjawascavBREAK" + 
+				"DconsularBREAK" + 
+				"DjkaBREAK" + 
+				"DjediknightguardianBREAK" + 
+				"DjklBREAK" + 
+				"DjkrBREAK" + 
+				"DjoleeBREAK" + 
+				"DjuhaniBREAK" + 
+				"DjynBREAK" + 
+				"Dk2soBREAK" + 
+				"DkananBREAK" + 
+				"DkamBREAK" + 
+				"DkitfistoBREAK" + 
+				"DkylorenBREAK" + 
+				"DkruBREAK" + 
+				"Dl3BREAK" + 
+				"DlandoBREAK" + 
+				"DlobotBREAK" + 
+				"DlograyBREAK" + 
+				"DfarmboyBREAK" + 
+				"DluminaraBREAK" + 
+				"DmaceBREAK" + 
+				"DmagmaBREAK" + 
+				"DmissionBREAK" + 
+				"DmobenforcerBREAK" + 
+				"DmonmothmaBREAK" + 
+				"DtalzanBREAK" + 
+				"DacolyteBREAK" + 
+				"DinitiateBREAK" + 
+				"DspiritBREAK" + 
+				"DzombieBREAK" + 
+				"DnuteBREAK" + 
+				"Dold benBREAK" + 
+				"DdakaBREAK" + 
+				"DpadmeBREAK" + 
+				"DpaoBREAK" + 
+				"DpaplooBREAK" + 
+				"DploBREAK" + 
+				"DpoeoldBREAK" + 
+				"DpoggleBREAK" + 
+				"DleiaBREAK" + 
+				"DqiraBREAK" + 
+				"DquigonBREAK" + 
+				"Dr2BREAK" + 
+				"DrangetrooperBREAK" + 
+				"DroloBREAK" + 
+				"DresherofinnBREAK" + 
+				"DresheropoeBREAK" + 
+				"DrespilotBREAK" + 
+				"DrestrooperBREAK" + 
+				"DgalacticreyBREAK" + 
+				"DjtrBREAK" + 
+				"DstickreyBREAK" + 
+				"DroseBREAK" + 
+				"DroyalguardBREAK" + 
+				"DsabineBREAK" + 
+				"DsavageBREAK" + 
+				"DshaakBREAK" + 
+				"DshoreBREAK" + 
+				"DsithBREAK" + 
+				"DsithempiretrooperBREAK" + 
+				"DmarauderBREAK" + 
+				"DsithtrooperBREAK" + 
+				"DsnowtrooperBREAK" + 
+				"DstormtrooperBREAK" + 
+				"DsthBREAK" + 
+				"DsunfacBREAK" + 
+				"DsupremekyloBREAK" + 
+				"Dt3BREAK" + 
+				"DtaliaBREAK" + 
+				"DteeboBREAK" + 
+				"DmandelorianBREAK" + 
+				"D3pacBREAK" + 
+				"DtfpBREAK" + 
+				"DtuskenraiderBREAK" + 
+				"DtuskenshamanBREAK" + 
+				"DugnaughtBREAK" + 
+				"DururururuurBREAK" + 
+				"DvandorBREAK" + 
+				"DvetchewieBREAK" + 
+				"DvethanBREAK" + 
+				"DvisasBREAK" + 
+				"DwampaBREAK" + 
+				"DwatBREAK" + 
+				"DwedgeBREAK" + 
+				"DwicketBREAK" + 
+				"DyounghanBREAK" + 
+				"DyounglandoBREAK" + 
+				"DzaalbarBREAK" + 
+				"DzamBREAK";
+		for(int i = 0; i < 10; i++)
+		{
+			int random = (int)(Math.random()*193);
+			int a = 0;
+			while(random != 0)
+			{
+				if(names.substring(a, a+5).equals("BREAK"))
+				{
+					--random;
+				}
+				++a;
+			}
+			String name = "";
+			a += 5;
+			while(!names.substring(a, a+5).equals("BREAK"))
+			{
+				name += names.substring(a, a+1);
+				++a;
+			}
+			System.out.println(name);
+		}
+		
+	}
+
+}
